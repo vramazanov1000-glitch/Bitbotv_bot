8634e4745c614e4785427fdf7a471e9025289d8f
vramazanov1000-glitch <vramazanov1000@gmail.com>
2026-08-09 22:54:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import telebot
 # Получаем токен из переменных окружения Render
 TOKEN = os.getenv('BOT_TOKEN')
 
-# Проверка, что токен вообще подгрузился
+# Проверка, что токен подгрузился
 if not TOKEN:
     raise ValueError("Не задан BOT_TOKEN в переменных окружения!")
 
@@ -12,7 +12,11 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Бот успешно запущен и работает! 🚀")
+    bot.reply_to(message, "Привет, Васиф! Бот успешно запущен и работает! 🚀")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, f"Ты написал: {message.text}")
 
 if __name__ == '__main__':
     print("Бот запущен и ожидает сообщения...")
